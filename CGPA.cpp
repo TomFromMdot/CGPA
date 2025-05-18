73e73b7d358f84bac1e76acb3b7f3ec10bbbfc8b
TomFromMdot <tomaszparda.dev@gmail.com>
2025-05-18 13:06:11 +0200

    Deleted Polish comments...

--- CGPA.cpp
+++ CGPA.cpp
@@ -90,7 +90,6 @@ public:
     * 
     * @return If find user then return week_ptr User else return nullopt.
     */
-
     std::optional<std::weak_ptr<User>> getUser(const std::string& username)
     {
         for (auto& user : usersList)
@@ -141,12 +140,6 @@ public:
 
 int main()
 {
-    //Initializacja Systemu CGPA
-    //Utworzenie użytkowników
-    //Dodanie ich do listy użytkwoników
-
-    //Przypisanie przypisanie ocen do użytkowników
-    //Pobranie liczby CPGA od użytkownika
 
 
     UsersData uData;
@@ -162,14 +155,3 @@ int main()
 
     uData.printUsers();
 }
-
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
